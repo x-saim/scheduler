7e2e5c9b3f15093e8faa835d4700f681551e3de9
Saim Amir <saimxamir@gmail.com>
2023-06-23 22:21:39 -0400

    add event handler and props to DayListItem

--- stories/index.js
+++ stories/index.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import DayListItem from "components/DayListItem";
+
 
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem";
 
 storiesOf("Button", module)
   .addParameters({
