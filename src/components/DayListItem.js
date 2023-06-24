28769c31e9a5c79266eb93041be8fc3de75362f7
Saim Amir <saimxamir@gmail.com>
2023-06-24 18:11:43 -0400

    update controlled list naming convention

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,7 @@ import 'components/DayListItem.scss'
 
 
 
-export default function DayListItem({ spots, selected, id, setDay, name }) {
+export default function DayListItem({ spots, selected, setDay, name }) {
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': selected,
     'day-list__item--full': (spots === 0)
@@ -17,7 +17,7 @@ export default function DayListItem({ spots, selected, id, setDay, name }) {
 
   return (
     <li className={dayClass}
-      onClick={setDay}>
+      onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots({ spots })} remaining</h3>
     </li >
