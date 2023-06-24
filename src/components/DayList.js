1f03a498a8b6de05b55c9c7dffbdd6ff1d97eaf0
Saim Amir <saimxamir@gmail.com>
2023-06-24 01:24:45 -0400

    complete creating DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,20 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList() {
+export default function DayList({ days, day, setDay }) {
 
+  const data = days.map((e) => {
+    return <DayListItem
+      key={e.id}
+      name={e.name}
+      spots={e.spots}
+      selected={e.name === day}
+      setDay={e.setDay} />
+  })
 
   return (
     <ul>
-      <DayListItem />
+      {data}
     </ul>
   )
 }
