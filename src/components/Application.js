7e5ee89c49e65e2d715730a2760ce284da88cbbf
Saim Amir <saimxamir@gmail.com>
2023-06-25 02:56:50 -0400

    convert appointments obj to arr and map it

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -22,9 +22,55 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  const appointmentsArray = Object.entries(appointments);
+  console.log(appointmentsArray)
+  const appointment = appointmentsArray.map((a) => {
+    return <Appointment key={a[1].id} time={a[1].time} interview={a[1].interview} />
+  })
+
 
   return (
     <main className="layout">
@@ -52,7 +98,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key={"last"} time={"5pm"} />
       </section>
 
     </main>
