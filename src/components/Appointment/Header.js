35a4bfe64c2293c920a04775ec5637d2ae47186c
Saim Amir <saimxamir@gmail.com>
2023-06-25 00:31:11 -0400

    refactor Header component to use props

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-export default function Header() {
+export default function Header({ time }) {
 
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{time}</h4>
       <hr className="appointment__separator" />
     </header>
   )
