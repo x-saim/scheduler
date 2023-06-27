84229f9b5246dd0af95d47550d017df5b9159aba
Saim Amir <saimxamir@gmail.com>
2023-06-27 06:09:47 -0400

    add transition with replace to custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,9 +5,20 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
     setMode(newMode)
-    setHistory(prevHistory => [...prevHistory, newMode])
+
+    if (replace) {
+      //using previous state value of history. Updates last element of updatedHistory to newMode by accessing the last index of updatedHistory array.Lastly, the updated updatedHistory array is returned from the callback function, which will be used by setHistory to update the state value of history.
+
+      setHistory((prevHistory) => {
+        const updatedHistory = [...prevHistory];
+        updatedHistory[updatedHistory.length - 1] = newMode;
+        return updatedHistory;
+      })
+    } else {
+      setHistory(prevHistory => [...prevHistory, newMode])
+    }
   };
 
   //checks if the history array has more than one item. If it does, it creates a new updatedHistory array by excluding the last item using slice().
