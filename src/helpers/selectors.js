f39e91e676003179d44401f8968a8ea1c72bff71
Saim Amir <saimxamir@gmail.com>
2023-06-27 03:48:27 -0400

    add selector test for getInterview()

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -31,3 +31,23 @@ export function getAppointmentsForDay(state, day) {
   return arr
 }
 
+/*The function should return a new object containing the interview data when we pass it an object that contains the interviewer.
+
+Otherwise, the function should return null. The object it returns should look like this:
+
+{  
+  "student": "Lydia Miller-Jones",
+  "interviewer": {  
+    "id": 1,
+    "name": "Sylvia Palmer",
+    "avatar": "https://i.imgur.com/LpaY82x.png"
+  }
+}
+
+*/
+export function getInterview(state, interview) {
+
+
+
+
+}
