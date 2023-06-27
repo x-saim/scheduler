5a27627a3d14a5e50947ce275f7a9878186197b2
Saim Amir <saimxamir@gmail.com>
2023-06-26 22:26:17 -0400

    install axios@0.20.0 and import useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
