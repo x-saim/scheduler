3c932adacb4bdf33b791059add9a580468e8b63c
Saim Amir <saimxamir@gmail.com>
2023-07-04 06:06:43 -0400

    set up test file for Appointment component

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment";
+
+afterEach(cleanup);
+
+
+it("renders without crashing", () => {
+  render(<Appointment />);
+});
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -8,10 +8,9 @@ export default function useVisualMode(initial) {
   const transition = (newMode, replace = false) => {
     setMode(newMode)
 
+    /*using previous state value of history. Updates last element of updatedHistory to newMode by accessing the last index of updatedHistory array.Lastly, the updated updatedHistory array is returned from the callback function, which will be used by setHistory to update the state value of history.*/
     setTimeout(() => {
       if (replace) {
-        //using previous state value of history. Updates last element of updatedHistory to newMode by accessing the last index of updatedHistory array.Lastly, the updated updatedHistory array is returned from the callback function, which will be used by setHistory to update the state value of history.
-
         setHistory((prevHistory) => {
           const updatedHistory = [...prevHistory];
           updatedHistory[updatedHistory.length - 1] = newMode;
@@ -27,7 +26,8 @@ export default function useVisualMode(initial) {
   //checks if the history array has more than one item. If it does, it creates a new updatedHistory array by excluding the last item using slice().
   const back = () => {
     if (history.length > 1) {
-      const updatedHistory = history.slice(0, -1);
+      const updatedHistory = [...history];
+      updatedHistory.pop(); // Remove the last mode from history
       setMode(updatedHistory[updatedHistory.length - 1]);
       setHistory(updatedHistory);
     }
