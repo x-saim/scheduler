0cc0d8af0776d6498bfde04526e7547f3bd025ed
Saim Amir <saimxamir@gmail.com>
2023-07-10 01:17:20 -0400

    add new test and describe block to Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,12 +6,36 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
 
-  const { getByText } = render(<Application />);
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
 
-  await waitForElement(() => getByText("Monday"))
+    const { getByText } = render(<Application />);
 
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+    await waitForElement(() => getByText("Monday"))
+
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview, and reduces the spots remaining for the first day by 1", async () => {
+
+    //Render the Application component.
+    const { getByText, getByPlaceholderText } = render(<Application />)
+
+    //Wait until the text 'Archie Cohen' is displayed
+    await waitForElement(() => getByText("Archie Cohen"))
+
+    //Click the "Add" button on the first empty appointment
+    fireEvent.click(getByText("Add"));
+
+    //Enter the name "lydia Miller-Jones" into the input with the placeholder "Enter Student Name"
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+
+  });
+
+})
