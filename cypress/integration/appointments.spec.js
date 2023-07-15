65576758f446aa803249947cd4ab4d668680918f
Saim Amir <saimxamir@gmail.com>
2023-07-15 06:46:16 -0400

    refactor test code and add comments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,60 +1,55 @@
 describe("Appointment", () => {
 
+  // Run this before each test case
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
-
     cy.visit("/");
-
     cy.contains("Monday");
   });
 
+  // Helper function to create an appointment
+  const createAppointment = () => {
+    cy.get("[alt=Add]").first().click(); // Click the "Add" button
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones"); // Enter the student name
+    cy.get('[alt="Sylvia Palmer"]').click(); // Select the interviewer
+    cy.contains("Save").click(); // Click the "Save" button
+  };
+
+  // Helper function to edit an appointment
+  const editAppointment = () => {
+    cy.get(".appointment__card").trigger("mouseover"); // Hover over the appointment card
+    cy.get("[alt='Edit']").click({ force: true }); // Click the "Edit" button with forced action
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones"); // Clear and enter the updated student name
+    cy.get('[alt="Tori Malcolm"]').click(); // Select the new interviewer
+    cy.contains("Save").click(); // Click the "Save" button
+  };
+
+  // Helper function to cancel an appointment
+  const cancelAppointment = () => {
+    cy.get(".appointment__card").trigger("mouseover"); // Hover over the appointment card
+    cy.get("[alt='Delete']").click({ force: true }); // Click the "Delete" button with forced action
+    cy.contains("Confirm").click(); // Click the "Confirm" button
+    cy.contains("Deleting").should("exist"); // Verify that the "Deleting" indicator exists
+    cy.contains("Deleting").should("not.exist"); // Verify that the "Deleting" indicator does not exist
+  };
+
+  // Test case: should book an interview
   it("should book an interview", () => {
-    cy.get("[alt=Add]")
-      .first()
-      .click();
+    createAppointment(); // Create an appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones"); // Verify the presence of the student name
+    cy.contains(".appointment__card--show", "Sylvia Palmer"); // Verify the presence of the interviewer name
+  });
 
-    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    cy.get('[alt="Sylvia Palmer"]').click();
-
-    cy.contains("Save").click()
-
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
-  })
-
+  // Test case: should edit an interview
   it("should edit an interview", () => {
-    // Hover over the appointment card
-    cy.get(".appointment__card").trigger("mouseover");
-
-    // Click the edit button with forced action
-    cy.get("[alt='Edit']").click({ force: true });
-
-    //Clear and change input field to Lydia Miller-Jones
-    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
-
-    //Select new interviewer "Tori Malcom"
-    cy.get('[alt="Tori Malcolm"]').click();
-    cy.contains("Save").click();
-
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Tori Malcolm");
+    editAppointment(); // Edit an existing appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones"); // Verify the updated student name
+    cy.contains(".appointment__card--show", "Tori Malcolm"); // Verify the updated interviewer name
   });
 
+  // Test case: should cancel an interview
   it("should cancel an interview", () => {
-    // Hover over the appointment card
-    cy.get(".appointment__card").trigger("mouseover");
-
-    // Click the edit button with forced action
-    cy.get("[alt='Delete']").click({ force: true });
-
-    //
-    cy.contains("Confirm").click();
-
-    cy.contains("Deleting").should("exist")
-    cy.contains("Deleting").should("not.exist")
-
-    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
-
+    cancelAppointment(); // Cancel an existing appointment
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist"); // Verify the absence of the canceled appointment
   });
-
 });
