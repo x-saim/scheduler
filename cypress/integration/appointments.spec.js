2852afc4d664b94b1be9c9e1e71f8c668786fe2e
Saim Amir <saimxamir@gmail.com>
2023-07-15 06:16:07 -0400

    refactor code to separate common behaviour of tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,18 +1,21 @@
 describe("Appointment", () => {
+
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+
+    cy.visit("/");
+
+    cy.contains("Monday");
+  });
+
   it("should book an interview", () => {
-    cy.request("GET", "/api/debug/reset")
-
-    cy.visit("/")
-
-    cy.contains("[data-testid=day]", "Monday")
-      .click()
-      .get("[alt=Add]")
+    cy.get("[alt=Add]")
       .first()
       .click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-
     cy.get('[alt="Sylvia Palmer"]').click();
+
     cy.contains("Save").click()
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
