78efb60af57b074eb8864d6f727f4819c3991296
Saim Amir <saimxamir@gmail.com>
2023-07-15 06:31:38 -0400

    add test for editing an appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -21,4 +21,22 @@ describe("Appointment", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   })
+
+  it("should edit an interview", () => {
+    // Hover over the appointment card
+    cy.get(".appointment__card").trigger("mouseover");
+
+    // Click the edit button with forced action
+    cy.get("[alt='Edit']").click({ force: true });
+
+    //Clear and change input field to Lydia Miller-Jones
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+
+    //Select new interviewer "Tori Malcom"
+    cy.get('[alt="Tori Malcolm"]').click();
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
